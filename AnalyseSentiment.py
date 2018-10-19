311391ea5201b887b569c2bf542d0d3900be4ad6
chiheb <chiheb.elouekdi@gmail.com>
2018-10-18 22:01:50 -0400

    permiers tests

--- AnalyseSentiment.py
+++ AnalyseSentiment.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+# ########################################################################################################################
+#                                                                                                                      #
+# AUTEUR: CHIHEB EL OUEKDI
+#  IFT - 7022
+#  TRAVAIL-2
+# DATE:                                                                                                 #
+########################################################################################################################
+
+
 from nltk.corpus import wordnet as wn
 import nltk
 from nltk.stem import PorterStemmer
@@ -5,25 +15,28 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 import os
 
 
+import re
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
 rep_pos = "Book/pos_Bk/"
 rep_neg = "Book/neg_Bk/"
 fichier = open("Book/pos_Bk/pos288.text")
 contenu = fichier.read()
-
+ps = PorterStemmer()
 nb_fichier =0
 for fichier in os.listdir(rep_pos):
     if fichier.endswith(".text"):
         f = open(rep_pos+fichier,"r")
-        print fichier
-        print os.path.dirname(fichier)
-        contenu = f.read()
-        print contenu
-        nb_fichier = nb_fichier+1
-#        words = word_tokenize(contenu)
-#        ps = PorterStemmer()
+        print rep_pos+fichier
+        tokens = word_tokenize(f.read())
 
-#        for w in words:
-#            print(w,ps.stem(w))
+        for t in tokens:
+            print rep_pos+fichier
+            print t
+            print(ps.stem(t))
 
 print nb_fichier
 #nltk.download('wordnet')
