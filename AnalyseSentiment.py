9ef306625eb968f47549283b4103d496ee5b3a4f
chiheb <chiheb.elouekdi@gmail.com>
2018-10-24 20:00:34 -0400

    permiers tests

--- AnalyseSentiment.py
+++ AnalyseSentiment.py
@@ -8,14 +8,15 @@
 ########################################################################################################################
 
 
-from nltk.corpus import wordnet as wn
 import nltk
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet as wn
+from nltk.probability import *
 from nltk.stem import PorterStemmer
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import word_tokenize
 import os
+from nltk.stem import WordNetLemmatizer
 
-
-import re
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -23,20 +24,31 @@ sys.setdefaultencoding('utf8')
 
 rep_pos = "Book/pos_Bk/"
 rep_neg = "Book/neg_Bk/"
-fichier = open("Book/pos_Bk/pos288.text")
-contenu = fichier.read()
+
 ps = PorterStemmer()
+# préparation de la liste stop_words
+stop_words = set(stopwords.words('english'))
+
 nb_fichier =0
 for fichier in os.listdir(rep_pos):
     if fichier.endswith(".text"):
         f = open(rep_pos+fichier,"r")
-        print rep_pos+fichier
         tokens = word_tokenize(f.read())
-
+        freq_dist_unigram = FreqDist(tokens)
+        #  analyse grammaticale (POS tagging)
+        liste_postagging = nltk.pos_tag(tokens)
+        
         for t in tokens:
-            print rep_pos+fichier
-            print abc
-            print(ps.stem(t))
+
+            try:
+                # Normalisation des mots
+                wordnet_lemmatizer = WordNetLemmatizer()
+                w = wordnet_lemmatizer.lemmatize(ps.stem(t))
+            except ValueError:
+                print rep_pos + fichier
+                nb_fichier=nb_fichier+1
+
+
 
 print nb_fichier
 #nltk.download('wordnet')
